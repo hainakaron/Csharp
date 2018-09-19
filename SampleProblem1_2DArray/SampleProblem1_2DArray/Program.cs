d9e9907e8b1248fa0d65c6ef0861f8eb87f8bd65
Norhaina Karon <hainaalfonso@gmail.com>
2018-09-19 18:00:48 +0800

    2D Array Code
    Added 2D array sample problem code

---
+++ SampleProblem1_2DArray/SampleProblem1_2DArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProblem1_2DArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//row will be used in the loop to access the row elements
+			//column will be used in the loop to access the column elements
+			//counter will be used to access the one-dimensional array elements
+			int row, column, counter=0;
+
+			//initializing the 3x5 array
+			int[,] number = {
+				{ 1, 2, 3, 4, 5 },
+				{ 6, 7, 8, 9, 10 },
+				{ 11, 12, 13, 14, 15 }
+			};
+
+			//assigning the content of the two-dimensional array
+			//to the one-dimensional array
+			int[] oneDim = new int[number.Length];
+			for (row = 0; row < number.GetLength(0); row++)
+			{
+				for (column = 0; column < number.GetLength(1); column++)
+				{
+					oneDim[counter] = number[row, column];
+					counter++;
+				}
+			}
+
+			//displaying the one-dimensional array
+			Console.WriteLine("Displaying the one-dimensional array in one line");
+			for (counter = 0; counter < oneDim.Length; counter++)
+			{
+				Console.Write(oneDim[counter] + "\t");
+			}
+			Console.WriteLine();
+
+			//displaying the two-dimensional array
+			Console.WriteLine("Displaying the two-dimensional array in table form");
+			for (row = 0; row < number.GetLength(0); row++)
+			{
+				for (column = 0; column < number.GetLength(1); column++)
+				{
+					Console.Write(number[row,column] + "\t");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			Console.Write("Press any key to exit...");
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ SampleProblem2_2DArray/SampleProblem2_2DArray/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProblem2_2DArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[][] array = new int[5][]; //storage of the arrays
+			int[] numbers = new int[5]; //array for the user input
+			int x, row, column; //variables for my for loops
+
+			//ask user for 5 integer numbers
+			//then store them in the array
+			Console.WriteLine("Please enter 5 integer numbers ranging from 1-5: ");
+			for (x = 0; x < numbers.Length; x++)
+			{
+				numbers[x] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			for (row = 0; row < numbers.Length; row++)
+			{
+				int[] data = new int[numbers[row]]; //create separate arrays for the values
+
+				int total = 1; 
+				for (column = 0; column < data.Length; column++)
+				{
+					total = total * numbers[row]; //numbers raised to their incremented value
+					data[column] = total; //place the numbers in the array
+				}
+
+				array[row] = data; //place the new array to the jagged array
+			}
+
+			//display the content of the jagged array
+			Console.WriteLine("Final output:");
+			for (row = 0; row < array.Length; row++)
+			{
+				for (column = 0; column < array[row].Length; column++)
+				{
+					Console.Write(array[row][column] + "\t");
+				}
+				Console.WriteLine();
+			}
+
+			Console.WriteLine("Press any key to continue");
+			Console.ReadLine();
+		}
+	}
+}
