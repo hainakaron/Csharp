d44452b731063e2102cf36c01778efe8df67e436
Norhaina Karon <hainaalfonso@gmail.com>
2018-09-25 12:37:35 +0800

    2D Array Code
    Added code for sample problem 4

---
+++ SampleProblem4_2DArray/SampleProblem4_2DArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProblem4_2DArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Please enter the dimension of the square array:");
+			int dim = Convert.ToInt32(Console.ReadLine());
+			int[,] array = new int[dim,dim];
+			int num;
+			for (int column = 0; column < dim; column++)
+			{
+				num = 0;
+				for (int row = column; row < dim; row++)
+				{
+					num++;
+					array[row, column] = num;
+					array[column, row] = num;
+				}
+			}
+
+			Console.WriteLine("View of the table");
+			for (int row = 0; row < dim; row++)
+			{
+				for (int col = 0; col < dim; col++)
+				{
+					Console.Write(array[row, col] + "\t");
+				}
+				Console.WriteLine();
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
