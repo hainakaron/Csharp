e4d70229134fdf475805e370b198bc2ac4a02c34
Norhaina Karon <hainaalfonso@gmail.com>
2018-10-05 10:31:30 +0800

    Quiz_Strings
    Added code for quiz on strings

---
+++ Quiz_Strings/Quiz_Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_Strings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//as the user for input
+			Console.Write("Please enter a possible palindrome:");
+			string word = Console.ReadLine();
+
+			//checking
+			bool palindrome = true;
+			int x = 0;
+			int y = word.Length - 1;
+			while (x != y && x < y)
+			{
+				if (word[x] != word[y])
+					palindrome = false;
+				x++;
+				y--;
+			}
+
+			//display the result
+			Console.WriteLine(palindrome);
+			Console.ReadLine();
+		}
+	}
+}
